3fc76b560979b3adde3b494140f6bcdc37e07544
Thomas Saied <tomas.temo77@gmail.com>
2024-03-22 14:29:49 +0200

    Add chat models and admin

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Chat, Message
 
-# Register your models here.
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = (
+        "sender",
+        "receiver",
+        "created_at",
+        "modified_at",
+    )
+    list_filter = (
+        "created_at",
+        "modified_at",
+    )
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = (
+        "chat",
+        "sender",
+        "receiver",
+        "sent_at",
+        "is_read",
+        "created_at",
+        "modified_at",
+    )
+    list_filter = (
+        "sent_at",
+        "is_read",
+        "created_at",
+        "modified_at",
+    )
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+UserModel = get_user_model()
+
+
+class Chat(models.Model):
+    sender = models.ForeignKey(
+        UserModel, related_name="chat_sender", on_delete=models.CASCADE
+    )
+    receiver = models.ForeignKey(
+        UserModel, related_name="chat_receiver", on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Chat between {self.sender} and {self.receiver}"
+
+
+class Message(models.Model):
+    chat = models.ForeignKey(
+        Chat, related_name="chat_messages", on_delete=models.CASCADE
+    )
+    sender = models.ForeignKey(
+        UserModel, related_name="sender_messages", on_delete=models.CASCADE
+    )
+    receiver = models.ForeignKey(
+        UserModel, related_name="receiver_messages", on_delete=models.CASCADE
+    )
+    content = models.TextField()
+    sent_at = models.DateTimeField(blank=True, null=True)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Message between {self.sender} and {self.receiver}"
