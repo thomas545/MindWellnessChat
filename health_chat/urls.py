ca9df985002de94b1a23c99589fbb01f4ed271ea
Thomas Saied <tomas.temo77@gmail.com>
2024-03-23 13:05:30 +0200

    fix API docs title and description

--- health_chat/urls.py
+++ health_chat/urls.py
@@ -24,11 +24,11 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title="Health Chat API",
         default_version="v1",
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
+        description="Health Chat APIs",
+        terms_of_service="",
+        contact=openapi.Contact(email="contact@healthchat.world"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
